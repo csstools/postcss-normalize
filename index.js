6e43fa5cf6ed01cb1b80999810bbd4e964267639
Efremov Alexey <lexich121@gmail.com>
2016-05-18 11:00:35 +0300

    Migrate: to async postcss api

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ var postcss = require('postcss'),
 
 module.exports = postcss.plugin('postcss-normalize', function () {
   return function(css) {
-
-    var normalize = fs.readFileSync(require.resolve('normalize.css/normalize.css'), 'utf8');
-    normalize = new CleanCSS().minify(normalize).styles;
-
-    css.prepend(normalize);
-
+    var normalizePath = require.resolve('normalize.css/normalize.css');
+    return new Promise(function(resolve, reject) {
+      fs.readFile(normalizePath, 'utf8', function(err, data) {
+        err ? reject(err) : resolve(data);
+      })
+    }).then(function(data) {
+      return new CleanCSS().minify(data).styles;
+    }).then(function(data) {
+      css.prepend(data);
+    });
   };
 });
